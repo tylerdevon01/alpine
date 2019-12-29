3bc2766836657026d63f6e4caa6bd44bbb99e78e
Tyler <tylerdevon01@gmail.com>
2019-12-28 23:51:09 -0500

    Add Bootstrap, home & dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,30 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # DEVISE
+  devise_for :users, skip: :all
+  devise_scope :user do
+    get     "login",                to: "devise/sessions#new",          as: :new_user_session
+    post    "login",                to: "devise/sessions#create",       as: :user_session
+    delete  "logout",               to: "devise/sessions#destroy",      as: :destroy_user_session
+    
+    get     "/users/password/new",  to: "devise/passwords#new",         as: :new_user_password
+    get     "/users/password/edit", to: "devise/passwords#edit",        as: :edit_user_password
+    patch   "/users/password",      to: "devise/passwords#update",      as: :user_password
+    put     "/users/password",      to: "devise/passwords#update"
+    post    "/users/password",      to: "devise/passwords#create"
+
+    get     "/users/cancel",        to: "devise/registrations#cancel",  as: :cancel_user_registration
+    get     "register",             to: "devise/registrations#new",     as: :new_user_registration
+    get     "/users/edit",          to: "devise/registrations#edit",    as: :edit_user_registration
+
+    patch   "/users",               to: "devise/registrations#update",  as: :user_registration
+    put     "/users",               to: "devise/registrations#update"
+    delete  "/users",               to: "devise/registrations#destroy"
+    post    "/users",               to: "devise/registrations#create"
+  end
+
+  # ROOT
   root to: "home#index"
+
+  get "/dashboard", to: "dashboard#index"
 end
