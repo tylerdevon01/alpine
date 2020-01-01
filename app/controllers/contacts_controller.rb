5158994075ba3caf52da2e039dfa2320597d15a3
Tyler <tylerdevon01@gmail.com>
2019-12-31 21:10:39 -0500

    Contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -17,10 +17,20 @@ class ContactsController < ApplicationController
         end
     end
     def edit
+        @contact = Contact.find_by(id: params[:id])
     end
     def update
+        @contact = Contact.find_by(id: params[:id])
+        if @contact.update(contact_params)
+            redirect_to @contact
+        else
+            render 'edit'
+        end
     end
     def destroy
+        @contact = Contact.find_by(id: params[:id])
+        @contact.destroy
+        redirect_to contacts_url
     end
 
 private
